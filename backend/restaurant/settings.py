fd0a3f6bf68ae60be669f477a6b6076a911b4bd0
flexiblecommits <flexcommits@gmail.com>
2023-09-15 08:13:15 +0800

    fixed the error of middleware

--- backend/restaurant/settings.py
+++ backend/restaurant/settings.py
@@ -43,15 +43,15 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.contrib.sessions.middleware.SessioMiddleware',
 ]
 
 ROOT_URLCONF = 'restaurant.urls'
